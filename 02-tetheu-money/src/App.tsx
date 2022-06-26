62dcd1e5bcd0e069a67af0e7f2ad15627976a94b
Matheus Phelipe <matheusphalves@gmail.com>
2022-06-26 16:08:40 -0300

    configurando styled components

---
+++ 02-tetheu-money/src/App.tsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from "./styles/global.ts";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello mundinho</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
